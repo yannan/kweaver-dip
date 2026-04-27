d70a0bc7c6e88a10be079fcc008fcc591444781c
duyq0519 <duyq0519@outlook.com>
2026-04-27 10:17:56 +0800

    fix: 去掉不合法的参数校验

--- dsg/services/apps/auth-service/common/dto/data_auth.go
+++ dsg/services/apps/auth-service/common/dto/data_auth.go
@@ -1,16 +1,15 @@
 package dto
 
-// DataResourceAuthReq 数据资源授权申请请求
+// DataResourceAuthReqArg 数据资源授权申请请求
 type DataResourceAuthReqArg struct {
-	ResouceID []string `json:"resouce_id" binding:"required,min=1,max=100,dive,uuid"` //申请资源ID列表
+	ResourceID []string `json:"resource_id" binding:"required,min=1,max=100,dive,uuid"` //申请资源ID列表
 	DataResourceAuthRequest
 }
 
 type DataResourceAuthRequest struct {
-	ApplyType      string   `json:"apply_type" binding:"required,oneof=check query"` //申请类型
-	Applicant      string   `json:"applicant" binding:"required,uuid"`               //申请人
-	ApplicantName  string   `json:"applicant_name" binding:"required"`               //申请人名称
-	ApplicantType  string   `json:"applicant_type" binding:"required"`               //申请人类型
-	AuthOperations []string `json:"auth_operations" binding:"omitempty"`             //授权操作列表
-	ExpiredAt      int64    `json:"expired_at" binding:"required,unix"`              //权限过期时间时间戳
+	Applicant      string   `json:"applicant" binding:"required,uuid"`   //申请人
+	ApplicantName  string   `json:"applicant_name" binding:"required"`   //申请人名称
+	ApplicantType  string   `json:"applicant_type" binding:"required"`   //申请人类型
+	AuthOperations []string `json:"auth_operations" binding:"omitempty"` //授权操作列表
+	ExpiredAt      int64    `json:"expired_at" binding:"required"`       //权限过期时间时间戳
 }
